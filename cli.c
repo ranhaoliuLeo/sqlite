986196d2acb4d314d28e7bad5dd271e1214bc3fe
ranhaoliu <ranhaoliu@tencent.com>
2021-12-21 11:43:01 +0800

    use header file

--- cli.c
+++ cli.c
@@ -4,48 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-
-typedef struct {
-    // buffer 输入缓冲指针
-    char *buffer;
-    // buffer_length 缓冲长度
-    size_t buffer_length;
-    // input_length 输入长度
-    ssize_t input_length;
-} InputBuffer;
-
-typedef enum {
-    META_COMMAND_SUCCESS,
-    META_COMMAND_UNKNOWN
-} MetaCommandResult;
-
-typedef enum {
-    PREPARE_SUCCESS,
-    PREPARE_UNKNOWN,
-} PrepareResult;
-
-typedef enum {
-    STATEMENT_INSERT,
-    STATEMENT_SELECT
-} StatementType;
-
-typedef struct {
-    StatementType type;
-} Statement;
-
-InputBuffer *new_input_buffer();
-
-void print_prompt() { printf("db > "); }
-
-void read_input(InputBuffer *inputBuffer);
-
-void closeInputBuffer(InputBuffer *inputBuffer);
-
-void executeStatement(Statement* statement);
-
-MetaCommandResult doMetaCommand(InputBuffer* inputBuffer);
-
-PrepareResult prepareStatement(InputBuffer* inputBuffer, Statement* statement);
+#include "cli.h"
 
 int main(int argc, char *argv[]) {
     InputBuffer *input_buffer = new_input_buffer();
